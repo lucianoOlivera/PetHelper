037bcae84021653c3e1abe6e25e7365f550e94ca
Maria Luz <marialuztejada055@gmail.com>
2021-08-12 18:48:08 -0300

    Revert "uploaded the base project"
    This reverts commit e79793832d02d61eee838c220eb0a34dbc2b5dbd.

--- bases/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BasesConfig(AppConfig):
-    name = 'bases'
--- bases/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from bases.views import Home
-from django.contrib.auth import views as auth_views 
-
-urlpatterns = [
-    path('', Home.as_view(),name='home'),
-    path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='bases/login.html'),name='logout'),
-]
