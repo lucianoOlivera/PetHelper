b815d802afcc33b0a067178a473067d56c404a5a
lucianoOlivera <oliveralucianof@gmail.com>
2021-08-15 21:56:33 -0300

    Create login and logout

--- bases/urls.py
+++ bases/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from bases.views import Home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
+    path
+    ('login/',
+     auth_views.LoginView.as_view(template_name='bases/login.html'),
+     name='login'),
+    path
+    ('login/',
+     auth_views.LogoutView.as_view(template_name='bases/login.html'),
+     name='logout'),
 ]
--- petHelper/urls.py
+++ petHelper/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include(('bases.urls','bases'), namespace= 'bases')),
+    path('', include(('bases.urls', 'bases'), namespace='bases')),
     path('admin/', admin.site.urls),
 ]
