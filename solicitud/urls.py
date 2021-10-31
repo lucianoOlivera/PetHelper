0e7321d10408b4c0b0aacd744c4eaba72f9ddf2d
luchoOlivera <oliveralucianof@gmail.com>
2021-10-31 19:35:31 -0300

    casi terminado

--- solicitud/urls.py
+++ solicitud/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from .views import SolicitudesListView, SolicitudDonacionInsumoNew, AddSolicitud, AddAsistencia
+from .views import SolicitudesListView, SolicitudDonacionInsumoNew
 
 urlpatterns = [
     path('solicitudes/', login_required(SolicitudesListView.as_view()), name='solicitudes_list'),
     path('solicitud_insumo/new', login_required(SolicitudDonacionInsumoNew.as_view()), name='solicitud_insumo_new'),
-    path('solicitudes/test', login_required(AddSolicitud.as_view()), name='solicitudes_add'),
-    path('solicitudes/test2', login_required(AddAsistencia.as_view()), name='solicitudes_add2'),
     ]
--- solicitud/views.py
+++ solicitud/views.py
@@ -15,8 +15,7 @@ from django.forms.models import inlineformset_factory, modelformset_factory
 from django.views.generic.edit import FormView, UpdateView
 #  fields=('titulo', 'pedido', 'veterinario',)
 InsumoFormset = inlineformset_factory(
-   Solicitud_Donacion_Insumo, Cantidad_Insumo, fields=('insumo', 'cantidad', 'solicitud_insumo',)
-)
+   Solicitud_Donacion_Insumo, Cantidad_Insumo, extra=5, fields=('insumo', 'cantidad', 'solicitud_insumo',))
 
 class SolicitudesListView(TemplateView):
     template_name = "solicitud/solicitud_list.html"
@@ -37,8 +36,6 @@ class SolicitudDonacionInsumoNew(SuccessMessageMixin, generic.CreateView):
     template_name = 'solicitud/solicitud_insumo_form.html'
     context_object_name = 'solicitud'
     fields = ['titulo', 'pedido', 'veterinario']
-    # form_class = SolicitudDonacionInsumoForm
-    # second_form_class = CantidadDeInsumo
     success_url = reverse_lazy('solicitud:solicitudes_list')
     success_message = "Solicitud creada sastifactoriamente"
     
@@ -59,36 +56,6 @@ class SolicitudDonacionInsumoNew(SuccessMessageMixin, generic.CreateView):
             Insumo.instance = self.object
             Insumo.save()
         return super().form_valid(form)
-
-
-class AddSolicitud(FormView):
-    template_name = 'solicitud/add.html'
-    form_class = formset_factory(Solicitud_Donacion_Insumo, extra=3)
-    success_url = '.'
-    
-    def form_valid(self, form): 
-
-        for f in form:
-            print(f.cleaned_data['full_name'])
-            f.save()
-
-        return super(AddSolicitud, self).form_valid(form)
-
-
-class AddAsistencia(FormView):
-    template_name = 'solicitud/asistencia.html'
-    form_class = inlineformset_factory(Solicitud_Donacion_Insumo, Cantidad_Insumo, fields=('insumo', 'cantidad', 'solicitud_insumo',))
-    success_url = '/'
-    context_object_name = 'solicitud'
-    fields = ['titulo', 'pedido', 'veterinario']
-
-    def form_valid(self, form):
-        # cuando se trabaja con model formset no es necesario hacer la iteracion
-        # este puede guardar de golpe todo el conjunto de formularios
-        #
-        form.save() 
-        return super(AddAsistencia, self).form_valid(form)
-
 
 
 # class SolicitudDonacionInsumoDel(SuccessMessageMixin, generic.DeleteView):
