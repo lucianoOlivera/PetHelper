78b7749ff29a45bf6c253c0ebda1963ef225cfb6
MLuzTejada <marialuztejada055@gmail.com>
2021-11-01 09:50:55 -0300

    verificacion de la cuenta con envio de email

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,7 +1,8 @@
+from django.conf.urls import include
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
-from usuario.views import RegistroUsuario, UsuarioDetail, UsuarioEdit, UsuarioPasswordEdit, UsuarioPasswordReset, UsuarioPasswordResetComplete, UsuarioPasswordResetConfirm, UsuarioPasswordResetDone
+from usuario.views import RegistroUsuario, UsuarioDetail, UsuarioEdit, UsuarioPasswordEdit, UsuarioPasswordReset, UsuarioPasswordResetComplete, UsuarioPasswordResetConfirm, UsuarioPasswordResetDone, VerificarEmailMensage
 
 urlpatterns = [
       path('registrar/', RegistroUsuario.as_view(), name='registrar'),
@@ -12,4 +13,6 @@ urlpatterns = [
       path('resetear_contraseña_confirm/<uidb64>/<token>', UsuarioPasswordResetConfirm.as_view(), name='password_reset_confirm'),
       path('resetear_contraseña_sent/', UsuarioPasswordResetDone.as_view(), name='password_reset_done'),
       path('resetear_contraseña_complete/', UsuarioPasswordResetComplete.as_view(), name='password_reset_complete'),
+      path('verification/', include('verify_email.urls')),
+      path('verification/confirm', VerificarEmailMensage.as_view(), name='verification_mensage'),
 ]
