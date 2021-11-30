854cb2a40ad0de17deeb6f694a8f63503f0767f1
MLuzTejada <marialuztejada055@gmail.com>
2021-11-30 20:55:21 -0300

    fixes generales

--- organizaciones/urls.py
+++ organizaciones/urls.py
@@ -10,16 +10,19 @@ urlpatterns = [
     path('organizaciones/delete/<int:pk>', login_required(OrganizacionDel.as_view()), name='organizaciones_del'),
     path('organizaciones/edit/<int:pk>', login_required(OrganizacionEdit.as_view()), name='organizaciones_edit'),
     path('organizaciones/profile/<int:pk>', login_required(OrganizacionDetail.as_view()), name='organizaciones_profile'),
+    
     path('clinicas/', login_required(ClinicaView.as_view()), name='clinicas_list'),
     path('clinicas/new', login_required(ClinicaNew.as_view()), name='clinicas_new'),
     path('clinicas/delete/<int:pk>', login_required(ClinicaDel.as_view()), name='clinicas_del'),
     path('clinicas/edit/<int:pk>', login_required(ClinicaEdit.as_view()), name='clinicas_edit'),
     path('clinicas/profile/<int:pk>', login_required(ClinicaDetail.as_view()), name='clinicas_profile'),
+    
     path('veterinarios/', login_required(VeterinarioView.as_view()), name='veterinarios_list'),
     path('veterinarios/new', login_required(VeterinarioNew.as_view()), name='veterinarios_new'),
     path('veterinarios/delete/<int:pk>', login_required(VeterinarioDel.as_view()), name='veterinarios_del'),
     path('veterinarios/edit/<int:pk>', login_required(VeterinarioEdit.as_view()), name='veterinarios_edit'),
     path('veterinarios/profile/<int:pk>', login_required(VeterinarioDetail.as_view()), name='veterinarios_profile'),
+    
     path('profesionales/', login_required(VeterinarioClinicaView.as_view()), name='profesionales_list'),
     path('profesionales/veterinarios', login_required(VeterinarioFilterView.as_view()), name='filter_veterinarios'),
     path('profesionales/clinicas', login_required(ClinicaFilterView.as_view()), name='filter_clinicas'),
