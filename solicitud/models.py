1e88251dc18ae8ace6900cb204d7734c30041c75
luchoOlivera <oliveralucianof@gmail.com>
2021-12-11 18:35:46 -0300

    nuevo flujo

--- solicitud/models.py
+++ solicitud/models.py
@@ -18,6 +18,7 @@ class Solicitud_Donacion_Insumo(ClaseModelo):
     titulo = models.TextField(max_length=100, null=False, blank=False)
     pedido = models.ImageField(upload_to="solicitud",blank=True, null=True)
     veterinario = models.ForeignKey(Veterinario, on_delete=models.CASCADE, null=True)
+    EstadoSolicitudInsumo = models.PositiveSmallIntegerField(default="0")
      
     def save(self):
         super(Solicitud_Donacion_Insumo, self).save()
@@ -35,6 +36,7 @@ class Solicitud_Donacion_Monetaria(ClaseModelo):
     alias = models.CharField(max_length=100, blank=True, null=True)
     nombre_titular = models.CharField(max_length=100, blank=True, null=True)
     nombre_banco = models.PositiveSmallIntegerField(choices=BANCOS, default="1")
+    EstadoSolicitudMonetaria = models.PositiveSmallIntegerField(default="0")
      
     def save(self):
         super(Solicitud_Donacion_Monetaria, self).save()
